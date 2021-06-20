4ad542480a7bd2a0c0d4ce1d4c483b9d4e3cc10c
Josh Petersen <jdpetersen92@gmail.com>
2021-06-20 12:09:34 -0700

    Added form submit listener. gradded input values

--- script.js
+++ script.js
@@ -32,3 +32,20 @@ const pizza = {
 		}
 	]
 };
+
+// listen for a form submit
+
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	// Once form submits, grab values from selected radio inputs
+	const sizeSelected = document.querySelector('input[name=size]:checked').value;
+	const priceSelected = document.querySelector('input[name=price]:checked').value;
+	
+	console.log(sizeSelected, priceSelected)
+
+})
+
+// use radio input values, to look through pizza object.
+// display the matching pizza joint to user.
+
